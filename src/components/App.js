cf80fd5b850d3a392ec6a258f97919fb7e54ccc3
Choudhary8055 <80520509+Choudhary8055@users.noreply.github.com>
2022-05-24 18:22:16 +0530

    aaa

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
-import React, { useReducer } from 'react'
-import { counterReducer } from '../reducers/counterReducer';
-import '../styles/App.css';
+import React, { useReducer } from "react";
+import { counterReducer } from "../reducers/counterReducer";
+import "../styles/App.css";
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+  const [state, dispatch] = useReducer(counterReducer, App);
   return (
     <div id="main">
-
+      <span id="counter">{state.count}</span>
+      <button
+        id="increment-btn"
+        onClick={() => dispatch({ type: "INCREMENTBYONE" })}
+      >
+        Increment
+      </button>
+      <br></br>
+      <button
+        id="decrement-btn"
+        onClick={() => dispatch({ type: "DECREMENTBYONE" })}
+      >
+        Decrement
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,21 @@
-const counterReducer = (state,action) =>{
+import React, { useReducer, useState } from "react";
 
-}
+const counterReducer = (state, action) => {
+  const initialValues = {
+    count: 0
+  };
+  switch (action.type) {
+    case "INCREMENTBYONE":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENTBYONE":
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+};
 
-export {counterReducer}
+export { counterReducer };
